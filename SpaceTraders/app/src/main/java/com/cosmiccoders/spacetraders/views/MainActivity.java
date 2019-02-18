801c75b57afd2eab1481a7330dbbae396528390b
skywheeler <li.hannah@gmail.com>
2019-02-17 22:03:14 -0500

    increment and decremet with conditions working

--- SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/MainActivity.java
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/MainActivity.java
@@ -42,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        shipField = findViewById(R.id.ship_name_field);
-        nameField = findViewById(R.id.name_field);
+        //shipField = findViewById(R.id.ship_name_field);
+        //nameField = findViewById(R.id.name_field);
 
         pilotSkills = findViewById(R.id.pilot_points);
         traderSkills = findViewById(R.id.trader_points);
@@ -62,35 +62,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSubtractPressed(View view) {
         Button change = (Button) findViewById(view.getId());
-        final TextView changeText = pilotSkills;
+        int temp = 0;
+        switch(view.getId()) {
+            case R.id.subtract_pilot:
+                temp = R.id.pilot_points;
+                break;
+            case R.id.subtract_trader:
+                temp = R.id.trader_points;
+                break;
+            case R.id.subtract_fighter:
+                temp = R.id.fighter_points;
+                break;
+            case R.id.subtract_engineer:
+                temp = R.id.engineer_points;
+        }
+        final TextView changeText = (TextView) findViewById(temp);
 
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch(v.getId()) {
                     case R.id.subtract_pilot:
-                        Log.d("Edit", "Subtracted");
-                        if (ps != 0) {
+                        if (ps > 0) {
                             ps--;
-                            changeText.setText(ps);
+                            changeText.setText("" + ps);
                         }
                     case R.id.subtract_trader:
-                        if(ts != 0) {
-                            Log.d("Edit", "Subtracted");
+                        if(ts > 0) {
                             ts--;
-                            traderSkills.setText(ts);
+                            changeText.setText("" + ts);
                         }
                     case R.id.subtract_fighter:
-                        if(fs != 0) {
-                            Log.d("Edit", "Subtracted");
+                        if(fs > 0) {
                             fs--;
-                            fighterSkills.setText(fs);
+                            changeText.setText("" + fs);
                         }
                     case R.id.subtract_engineer:
-                        Log.d("Edit", "Subtracted");
-                        if(es != 0) {
+                        if(es > 0) {
                             es--;
-                            engineerSkills.setText(ps);
+                            changeText.setText("" + es);
                         }
                 }
             }
@@ -98,15 +108,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAddPressed(View view) {
-        final int total = ts + ps + fs + es;
         Button change = (Button) findViewById(view.getId());
-        final TextView changeText = (TextView) findViewById(R.id.pilot_points);
-
+        int temp = 0;
+        switch(view.getId()) {
+            case R.id.add_pilot:
+                temp = R.id.pilot_points;
+                break;
+            case R.id.add_trader:
+                temp = R.id.trader_points;
+                break;
+            case R.id.add_fighter:
+                temp = R.id.fighter_points;
+                break;
+            case R.id.add_engineer:
+                temp = R.id.engineer_points;
+        }
+        final TextView changeText = (TextView) findViewById(temp);
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ps++;
-                changeText.setText("Yay!");
+                final int total = ts + ps + fs + es;
+                if(total < 16) {
+                    switch(v.getId()) {
+                        case R.id.add_pilot:
+                            ps++;
+                            changeText.setText("" + ps);
+                            break;
+                        case R.id.add_trader:
+                            ts++;
+                            changeText.setText("" + ts);
+                            break;
+                        case R.id.add_fighter:
+                            fs++;
+                            changeText.setText("" + fs);
+                            break;
+                        case R.id.add_engineer:
+                            es++;
+                            changeText.setText("" + es);
+                    }
+                }
             }
         });
     }
