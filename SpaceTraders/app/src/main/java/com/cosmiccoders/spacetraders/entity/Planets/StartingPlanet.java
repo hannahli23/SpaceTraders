b42d1a655aef1281a17abd74b01554a31f42e047
skywheeler <li.hannah@gmail.com>
2019-02-25 14:32:21 -0500

    Adding the starting planet

---
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/entity/Planets/StartingPlanet.java
@@ -0,0 +1,13 @@
+package com.cosmiccoders.spacetraders.entity.Planets;
+
+import com.cosmiccoders.spacetraders.entity.PlanetResources;
+import com.cosmiccoders.spacetraders.entity.PlanetTemp;
+import com.cosmiccoders.spacetraders.entity.TechLevel;
+
+public class StartingPlanet extends PlanetTemp{
+
+    public StartingPlanet(int[] location) {
+        super("Rolling Hills", location, TechLevel.ARGICULTURE,
+                PlanetResources.NoSpecialResources);
+    }
+}
