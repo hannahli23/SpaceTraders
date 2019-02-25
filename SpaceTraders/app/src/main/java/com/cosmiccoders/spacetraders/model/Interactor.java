3b361a0895569014d09d066a7f7b0d7700328e6a
brandonolson <brandonolson32@yahoo.com>
2019-02-25 16:12:56 -0500

    Added a planet to planet

--- SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/model/Interactor.java
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/model/Interactor.java
@@ -1,4 +1,10 @@
 package com.cosmiccoders.spacetraders.model;
 
 public class Interactor {
+
+    private Repository myRepo;
+
+    protected Interactor(Repository repo) {myRepo = repo;}
+
+    protected  Repository getRepo() {return myRepo;}
 }
