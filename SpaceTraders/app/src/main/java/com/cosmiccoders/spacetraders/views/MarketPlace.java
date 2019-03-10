037714492a9c5faf4e3d83693c92a3f6188a8687
skywheeler <li.hannah@gmail.com>
2019-03-09 22:48:03 -0500

    some connections

---
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/MarketPlace.java
@@ -0,0 +1,19 @@
+package com.cosmiccoders.spacetraders.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.cosmiccoders.spacetraders.R;
+
+public class MarketPlace extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.marketplace);
+    }
+}
