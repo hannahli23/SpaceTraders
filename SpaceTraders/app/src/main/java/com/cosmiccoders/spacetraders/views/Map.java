2abf2478e67fef13951b378dc4308df3c472c031
skywheeler <li.hannah@gmail.com>
2019-04-01 13:59:41 -0400

    some changes to planets

--- SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/Map.java
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/Map.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.cosmiccoders.spacetraders.R;
 import com.cosmiccoders.spacetraders.viewmodels.EditAddPlayerViewModel;
@@ -26,6 +27,12 @@ public class Map extends AppCompatActivity {
         setContentView(R.layout.planet_map);
         solarSystem = ViewModelProviders.of(this).get(ViewAddSolarSystemViewModel.class);
         planetViewModel = ViewModelProviders.of(this).get(GetSetPlanetViewModel.class);
+        shipViewModel = ViewModelProviders.of(this).get(EditShipViewModel.class);
+
+        TextView location = findViewById(R.id.location_tag);
+        TextView fuel = findViewById(R.id.fuel_amount);
+        location.setText(planetViewModel.getPlanet().getName());
+        fuel.setText(shipViewModel.getMainShip().getFuel()+"");
     }
     public void onPlanetPress(View v) {
         Button change = (Button) findViewById(v.getId());
