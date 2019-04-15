c55daa7d5857d415cf76c027a7868ba925b8c588
Olivia Amyette <oamyette3@gatech.edu>
2019-04-14 20:21:14 -0400

    JDs

--- SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/PlanetIntro.java
+++ SpaceTraders/app/src/main/java/com/cosmiccoders/spacetraders/views/PlanetIntro.java
@@ -56,13 +56,13 @@ public class PlanetIntro extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kravat);
-        /**
-         * You're going to be changing the textviews in on  create based on whichever planet is set
-         * as the planet you want to go to
-         * and then you should also have a click method for the GO button
-         * And in the go button you should be checking if the planet you want to go to is in
-         * the short range chart
-         * So is the check passes you should 1) reset main planet 2) naivgate to the shiphome page
+        /*
+          You're going to be changing the textviews in on  create based on whichever planet is set
+          as the planet you want to go to
+          and then you should also have a click method for the GO button
+          And in the go button you should be checking if the planet you want to go to is in
+          the short range chart
+          So is the check passes you should 1) reset main planet 2) naivgate to the shiphome page
          */
         solarSystem = ViewModelProviders.of(this).get(ViewAddSolarSystemViewModel.class);
         planetViewModel = ViewModelProviders.of(this).get(GetSetPlanetViewModel.class);
